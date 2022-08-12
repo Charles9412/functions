f7127a166e63278eb20e0c4e097f054436bfdcbb
Charles9412 <111099675+Charles9412@users.noreply.github.com>
2022-08-12 00:13:34 -0500

    Add files via upload
    numpy style documentation

--- scalers.py
+++ scalers.py
@@ -1,17 +1,25 @@
 import pandas as pd
 
 def scaler_nD(txt, sep, esc, newtxt):
-    """Takes a file into a DataFrame and multiplies the columns by a scalar, 
-    then prints head of DataFrame and saves it to a new file
+    """Takes a file into a DataFrame and multiplies the columns by a scalar,
+    then prints head of DataFrame
 
-    Args:
-        txt (.csv, .txt): File to import as DataFrame
-        sep (str): Type of delimiter of file
-        esc (int): Scalar to multiply on each column
-        newtxt (str): Name for the output file
+    Parameters
+    ----------
+    txt : .csv, .txt
+        File to import as DataFrame
+    sep : str
+        Type of delimiter of file
+    esc : int
+        Scalar to multiply on each column
+    newtxt : str
+        Name for the output file
 
-    Returns:
-        DataFrame : returns modified DataFrame
+    Returns
+    -------
+    DataFrame
+        returns modified DataFrame
+
     """
 
     data = pd.read_csv(txt, sep = sep, index_col = False)
