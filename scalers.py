b1774c2c533aeda30e0c085998a3822d6ce1a11e
Charles9412 <111099675+Charles9412@users.noreply.github.com>
2022-08-11 20:41:28 -0500

    Add files via upload
    Correction for scalers_nD commit.

--- scalers.py
+++ scalers.py
@@ -1,5 +1,28 @@
 import pandas as pd
 
-x=10
+def scaler_nD(txt, sep, esc, newtxt):
+    """Takes a file into a DataFrame and multiplies the columns by a scalar, 
+    then prints head of DataFrame
 
-print(x)
+    Args:
+        txt (.csv, .txt): File to import as DataFrame
+        sep (str): Type of delimiter of file
+        esc (int): Scalar to multiply on each column
+        newtxt (str): Name for the output file
+
+    Returns:
+        DataFrame : returns modified DataFrame
+    """
+
+    data = pd.read_csv(txt, sep = sep, index_col = False)
+
+    for col in data.columns:
+        data[col] = data[col] * esc
+
+    print(data.head())
+    data.to_csv(newtxt, sep='\t', index=False)
+
+    return data
+
+scaler_nD("vectortest2D.txt", "\t", 2, "newdata.txt")
+
